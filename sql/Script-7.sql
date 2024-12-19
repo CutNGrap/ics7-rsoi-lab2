a4fa24d96ee3c412161176a11db297776c02e26f
Alexey Vinogradov <grappess@mail.ru>
2024-12-20 02:20:38 +0300

    tests3

--- sql/Script-7.sql
+++ sql/Script-7.sql
@@ -39,3 +39,36 @@ CREATE TABLE cars
         CHECK (type IN ('SEDAN', 'SUV', 'MINIVAN', 'ROADSTER')),
     availability        BOOLEAN     NOT NULL
 );
+
+
+GRANT ALL PRIVILEGES ON table cars TO program;
+
+
+CREATE TABLE payment
+(
+    id          SERIAL PRIMARY KEY,
+    payment_uid uuid        NOT NULL,
+    status      VARCHAR(20) NOT NULL
+        CHECK (status IN ('PAID', 'CANCELED')),
+    price       INT         NOT NULL
+);
+
+GRANT ALL PRIVILEGES ON table payment TO program;
+
+
+
+CREATE TABLE rental
+(
+    id          SERIAL PRIMARY KEY,
+    rental_uid  uuid UNIQUE              NOT NULL,
+    username    VARCHAR(80)              NOT NULL,
+    payment_uid uuid                     NOT NULL,
+    car_uid     uuid                     NOT NULL,
+    date_from   TIMESTAMP WITH TIME ZONE NOT NULL,
+    date_to     TIMESTAMP WITH TIME ZONE NOT NULL,
+    status      VARCHAR(20)              NOT NULL
+        CHECK (status IN ('IN_PROGRESS', 'FINISHED', 'CANCELED'))
+);
+
+GRANT ALL PRIVILEGES ON table rental TO program;
+GRANT ALL PRIVILEGES ON table rental_id_seq TO program;
